51ff839229a295c860b9b986bd7cf6d45959c1e5
Robson-Teixeira <robsonmteixeira@outlook.com>
2023-11-09 00:47:22 -0300

    Orientação a Objetos: Segunda Instância

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -7,6 +7,14 @@ public class CriaConta {
 		primeiraConta.saldo = 200;
 		System.out.println("Primeira conta - Saldo: " + primeiraConta.saldo);
 
+		primeiraConta.saldo += 100;
+		System.out.println("Primeira conta - Valor recebido: " + 100d);
+		System.out.println("Primeira conta - Saldo: " + primeiraConta.saldo);
+
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		System.out.println("Segunda conta - Saldo: " + segundaConta.saldo);
+
 	}
 
 }
