1247ac5c14362f73dd35c6a7c18337cba9b663cf
Robson-Teixeira <robsonmteixeira@outlook.com>
2023-11-11 23:50:47 -0300

    Composição de objetos: Referência Null

---
+++ src/TestaContaSemCliente.java
@@ -0,0 +1,15 @@
+
+public class TestaContaSemCliente {
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		System.out.println("Conta - Saldo: " + conta.saldo);
+
+		System.out.println("Conta - Cliente: " + conta.cliente);
+
+		conta.cliente = new Cliente();
+		System.out.println("Conta - Cliente: " + conta.cliente);
+
+		conta.cliente.nome = "Robson Teixeira";
+		System.out.println("Conta > Cliente - Nome: " + conta.cliente.nome);
+	}
+}
